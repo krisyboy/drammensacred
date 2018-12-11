394793e34bcb11af81683d0ae2daffc2f9428099
Kris <kristoffertoivonen@gmail.com>
2018-12-12 00:33:37 +0100

    oppdatering

--- web3/views/mainpage.php
+++ web3/views/mainpage.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
-<html>
-
-<head lang="en">
+<html lang="en">
+<head>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="../css/main.css">
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css" />
+  <link rel="stylesheet" href="/web3/css/main.css">
   <title>Drammen Sacred Music Festival</title>
+</head>
+
+<body>
   <!-- Nabbar -->
   <nav class="navbar sticky-top navbar-expand">
 
@@ -53,15 +56,13 @@
       </ul>
     </div>
   </nav>
-</head>
-
-<body>
-
   <!-- Makeshift Jumbotron -->
   <div class="img-wrapper">
     <img class="img-fluid" src="/web3/root/img/newjumbo4.png"></img>
     <div class="img-overlay">
-      <button class="btn btn-xs btn-success my-btn">TICKETS AND SCHEDULE</button>
+      <a href="../root/pages/tickets.html">
+        <button class="btn btn-xs btn-success my-btn">TICKETS AND SCHEDULE</button>
+      </a>
     </div>
   </div>
   <br>
@@ -72,46 +73,76 @@
   <br>
   <br>
   <!-- Artists -->
-    <div class="container-fluid text-center artist-container">
-      <div class="row">
-        <div class="col-sm">
-          <img class="img-thumbnail img-artist" src="/web3/root/img/Beharie.png">
-        </div>
-        <div class="col-sm">
-          <img class="img-thumbnail img-artist" src="/web3/root/img/Beharie.png">
-        </div>
-        <div class="col-sm">
-          <img class="img-thumbnail img-artist" src="/web3/root/img/Beharie.png">
-        </div>
-      </div>
-      <br>
-      <br>
-      <div class="row">
-        <div class="col-sm">
-          <img class="img-thumbnail img-artist" src="/web3/root/img/Beharie.png">
-        </div>
-        <div class="col-sm">
-          <img class="img-thumbnail img-artist" src="/web3/root/img/Beharie.png">
-        </div>
-        <div class="col-sm">
-          <img class="img-thumbnail img-artist" src="/web3/root/img/Beharie.png">
-        </div>
+  <section class="gallery-block compact-gallery">
+
+      <div class="container artist-container">
+      <div class="row no-gutters">
+          <div class="col-md-6 col-lg-4 item zoom-on-hover">
+              <a class="lightbox" href="/web3/root/img/Behariepro.jpg">
+                  <img class="img-fluid image" src="/web3/root/img/Behariepro.jpg">
+                  <span class="description">
+                      <span class="description-heading">Beharie</span>
+                      <span class="description-body">A living room concert with a Pop</span>
+                  </span>
+              </a>
+          </div>
+          <div class="col-md-6 col-lg-4 item zoom-on-hover">
+              <a class="lightbox" href="/web3/root/img/Waed.jpg">
+                  <img class="img-fluid image" src="/web3/root/img/Waed.jpg">
+                  <span class="description">
+                      <span class="description-heading">Waed Bouhassoun</span>
+                      <span class="description-body">The silk voice of Syria</span>
+                  </span>
+              </a>
+          </div>
+          <div class="col-md-6 col-lg-4 item zoom-on-hover">
+              <a class="lightbox" href="/web3/root/img/Gabacho.jpg">
+                  <img class="img-fluid image" src="/web3/root/img/Gabacho.jpg">
+                  <span class="description">
+                      <span class="description-heading">Gabacho Maroc</span>
+                      <span class="description-body">The bridge between African and Western music</span>
+                  </span>
+              </a>
+          </div>
+          <div class="col-md-6 col-lg-4 item zoom-on-hover">
+              <a class="lightbox" href="/web3/root/img/Solveig.jpg">
+                  <img class="img-fluid image" src="/web3/root/img/Solveig.jpg">
+                  <span class="description">
+                      <span class="description-heading">Solveig Slettahjell</span>
+                      <span class="description-body">Enthralling poetich speech in drowning depths</span>
+                  </span>
+                  </a>
+              </div>
+          <div class="col-md-6 col-lg-4 item zoom-on-hover">
+              <a class="lightbox" href="/web3/root/img/Yogaforhjertet.jpg">
+                  <img class="img-fluid image" src="/web3/root/img/Yogaforhjertet.jpg">
+                  <span class="description">
+                      <span class="description-heading">Yoga for the heart</span>
+                      <span class="description-body">Meditation & Chanting</span>
+                  </span>
+              </a>
+          </div>
+          <div class="col-md-6 col-lg-4 item zoom-on-hover">
+              <a class="lightbox" href="/web3/root/img/Sanskriti.jpg">
+                  <img class="img-fluid image" src="/web3/root/img/Sanskriti.jpg">
+                  <span class="description">
+                      <span class="description-heading">Sanskriti Shrestha</span>
+                      <span class="description-body">Different living room, different soul, and all that Jazz</span>
+                  </span>
+              </a>
+          </div>
       </div>
     </div>
-    <br>
-    <br>
     <div class="col text-center">
       <div class="artistcall">
-        <button class="btn btn-xs btn-success">ARTISTS PAGE</button>
+        <a href="../root/pages/artists.html">
+          <button class="btn btn-xs btn-success">ARTISTS PAGE</button>
+        </a>
       </div>
     </div>
-
-  <br>
-  <br>
-  <br>
-  <hr>
-  <br>
-  <br>
+  </div>
+</section>
+<br>
 
   <!-- Blog call to action -->
   <div class="blogcall">
@@ -127,7 +158,9 @@
         </div>
       </div>
       <div class="col text-center">
-        <button class="btn btn-xs btn-success"> OUR BLOG</button>
+        <a href="../root/pages/blog.html">
+          <button class="btn btn-xs btn-success"> OUR BLOG</button>
+        </a>
       </div>
     </div>
   </div>
@@ -145,61 +178,66 @@
         </div>
       </div>
       <div class="col-sm text-center">
-        <button class="btn btn-xs btn-success"> ABOUT US</button>
+        <a href="../root/pages/about.html">
+          <button class="btn btn-xs btn-success"> ABOUT US</button>
+        </a>
       </div>
     </div>
   </div>
-</body>
 
-<footer class="main-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4">
-        <div class"logo">
-          <div class="logo">
-            <h6 class="text-white">Drammen Sacred Music festival</h6>
-            <div class="contact-details">
-              <p>Adress</p>
-              <p>Phone</p>
-              <p>Email</p>
+  <footer class="main-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class"logo">
+            <div class="logo">
+              <h6 class="text-white">Drammen Sacred Music festival</h6>
+              <div class="contact-details">
+                <p>Adress</p>
+                <p>Phone</p>
+                <p>Email</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="col-md-4">
-        <div class="menus d-flex">
-          <ul class="list-unstyled">
-            <li>
-              <a href="pages/blog.html">Tickets<a>
-            </li>
-            <li>
-              <a href="pages/blog.html">Sacred Decade<a>
-            </li>
-            <li>
-              <a href="pages/blog.html">Social media<a>
-            </li>
-            <li>
-              <a href="pages/blog.html">Work with us<a>
-            </li>
-          </ul>
-          <ul class="list-unstyled">
-            <li>
-              <a href="pages/blog.html">Blog<a>
-            </li>
-            <li>
-              <a href="pages/blog.html">About us<a>
-            </li>
-            <li>
-              <a href="pages/blog.html">Press<a>
-            </li>
-            <li>
-              <a href="pages/blog.html">Artists<a>
-            </li>
-          </ul>
+        <div class="col-md-4">
+          <div class="menus d-flex">
+            <ul class="list-unstyled">
+              <li>
+                <a href="pages/blog.html">Tickets<a>
+              </li>
+              <li>
+                <a href="pages/blog.html">Sacred Decade<a>
+              </li>
+              <li>
+                <a href="pages/blog.html">Social media<a>
+              </li>
+              <li>
+                <a href="pages/blog.html">Work with us<a>
+              </li>
+            </ul>
+            <ul class="list-unstyled">
+              <li>
+                <a href="pages/blog.html">Blog<a>
+              </li>
+              <li>
+                <a href="pages/blog.html">About us<a>
+              </li>
+              <li>
+                <a href="pages/blog.html">Press<a>
+              </li>
+              <li>
+                <a href="pages/blog.html">Artists<a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</footer>
-
+  </footer>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+  <script>
+      baguetteBox.run('.compact-gallery', { animation: 'slideIn'});
+  </script>
+</body>
 </html>
